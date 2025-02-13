47795a98c11b02687305f0f645e0cbcfdefac412
Patrice Roy <patrice.roy@doyondespres.com>
2025-02-13 15:35:34 -0500

    fix: creating api .env file

--- docker/up.sh
+++ docker/up.sh
@@ -3,6 +3,7 @@
 NETWORK=docupet
 
 cp .env.example .env
+cp ../src/.env.example ../src/.env
 
 if ! docker network inspect "${NETWORK}" >/dev/null 2>&1; then
     docker network create --driver bridge ${NETWORK}
